7a25b0a25b90e104a0049461ffa89eb52cdb98e5
purpen <purpen.w@gmail.com>
2016-09-06 11:32:09 +0800

    update company name.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
+           <h1>鳍源科技</h1>            <p>深圳鳍源科技有限公司是一家专注于海洋探索的科技创新公司，通过互联网理念和现代科研力量构建全新的海洋商业模式，系列产品主推消费及轻工业市场。公司以研发销售水下机器人为硬件载体，搭载互联网内容平台，聚集来自世界各地的好奇者，开拓神秘未知的海底视野。</p>
         </div>
     </div>
 </div>
