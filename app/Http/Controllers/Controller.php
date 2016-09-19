83193224f2c4e5b2260b064d8b4e10b9fff89a98
tianshuai <tianshuaiorc@sina.com>
2016-09-19 16:11:18 +0800

    add first

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
+// Log
+use Illuminate\Support\Facades\Log;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
