6b83f72a0f1d3f8360922c9450f0576b23425d52
tianshuai <tianshuaiorc@sina.com>
2016-09-20 15:32:03 +0800

    stuff

--- app/Http/Transformers/StuffTransformer.php
+++ app/Http/Transformers/StuffTransformer.php
@@ -16,6 +16,7 @@ class StuffTransformer extends TransformerAbstract
             'id' => $stuff->id,
             'content' => $stuff->content,
             'view_count' => $stuff->view_count,
+            'kind' => $stuff->kind,
             'like_count' => $stuff->like_count,
             'comment_count' => $stuff->comment_count,
             'user_id' => $stuff->user_id,
