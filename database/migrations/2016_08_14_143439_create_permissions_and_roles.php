0c7ec4d27cb9310089111e1b2db3f47b45e01d95
purpen <purpen.w@gmail.com>
2016-10-10 18:59:48 +0800

    debug table.

--- database/migrations/2016_08_14_143439_create_permissions_and_roles.php
+++ database/migrations/2016_08_14_143439_create_permissions_and_roles.php
@@ -27,41 +27,6 @@ class CreatePermissionsAndRoles extends Migration
             $table->string('description')->nullable();
             $table->timestamps();
         });
-        
-        Schema::create('permission_role', function (Blueprint $table) {
-            $table->integer('permission_id')->unsigned();
-            $table->integer('role_id')->unsigned();
-            
-            $table->foreign('permission_id')
-                ->references('id')
-                ->on('permissions')
-                ->onDelete('cascade');
-            
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles')
-                ->onDelete('cascade');
-            
-            $table->primary(['permission_id', 'role_id']);        
-        });
-        
-        Schema::create('role_user', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned();
-            $table->integer('role_id')->unsigned();
-            
-            $table->foreign('role_id')
-                ->references('id')
-                ->on('roles')
-                ->onDelete('cascade');
-            
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('Users')
-                ->onDelete('cascade');
-            
-            $table->primary(['role_id', 'user_id']);      
-        });
-        
     }
 
     /**
@@ -73,7 +38,5 @@ class CreatePermissionsAndRoles extends Migration
     {
         Schema::drop('roles');
         Schema::drop('permissions');
-        Schema::drop('permission_role');
-        Schema::drop('role_user');
     }
 }
