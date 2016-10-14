af6faa4c296aac5a57b1bb4ec43a3d9b87320aa1
purpen <purpen.w@gmail.com>
2016-10-14 15:02:11 +0800

    update stuff location.

---
+++ database/migrations/2016_10_13_203515_add_location_stuff_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLocationStuffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stuffs', function (Blueprint $table) {
+            // 纬度
+            $table->double('lat', 15, 8)->nullable();
+            // 经度
+            $table->double('lng', 15, 8)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stuffs', function (Blueprint $table) {
+            $table->drop_column('lat');
+            $table->drop_column('lng');
+        });
+    }
+}
