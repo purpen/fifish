41b8f8b1959eb06d2b2d4d2a94152dee78b9cb1a
purpen <purpen.w@gmail.com>
2016-11-25 00:24:18 +0800

    fix wap style

--- resources/views/wap/news.blade.php
+++ resources/views/wap/news.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.wap')
 
 @section('content')
-<div class="container-fluid block-news">
+<div class="container-fluid container-wap-news">
     
     <div class="row text-center">
         @foreach ($columns as $column)
-        <div class="col-md-6">
-            <div class="thumbnail card">
-                <a href="{{ $column->url }}" class="image" target="_blank">
-                    <img src="{{ $column->cover ? $column->cover->file->adpic : '' }}" alt="{{ $column->title }}">
-                </a>
-                <div class="info">
-                    <h3>
-                        <a href="" target="_blank">{{ $column->title }}</a>
-                    </h3>
-                    <p>
-                        <span class="text-blue">{{ $column->sub_title }}</span>
-                        <span class="pull-right text-time">{{ $column->summary }}</span>
-                    </p>
-              </div>
-            </div>
+        <div class="thumbnail card">
+            <a href="{{ $column->url }}" class="image" target="_blank">
+                <img src="{{ $column->cover ? $column->cover->file->adpic : '' }}" alt="{{ $column->title }}">
+            </a>
+            <div class="info">
+                <h3>
+                    <a href="{{ $column->url }}" target="_blank">{{ $column->title }}</a>
+                </h3>
+                <p>
+                    <span class="text-blue">{{ $column->sub_title }}</span>
+                    <span class="pull-right text-time">{{ $column->summary }}</span>
+                </p>
+          </div>
         </div>
         @endforeach
     </div>
