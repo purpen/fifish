3fb19d0de11ecfef5e7d410ce9eaf146ce5e580c
purpen <purpen.w@gmail.com>
2016-12-02 16:12:03 +0800

    debug upload image quantity

--- app/Http/Utils/ImageUtil.php
+++ app/Http/Utils/ImageUtil.php
@@ -98,7 +98,7 @@ class ImageUtil
                 $saveas = self::urlsafe_base64_encode($bucket.':'.$file_path.'$(ext)!wm');
                 $persistentOps = 'vframe/jpg/offset/1|imageView2/1/w/480/h/270/interlace/1/q/100|watermark/1/image/aHR0cDovL29lNXRrdWJjai5ia3QuY2xvdWRkbi5jb20vd2F0ZXJAMngucG5n/dissolve/70/gravity/SouthWest/dx/20/dy/20;vframe/jpg/offset/1|imageView2/1/w/120/h/67/interlace/1/q/100;avthumb/mp4/wmImage/'.$water_image.'/wmGravity/SouthWest/wmOffsetX/20/wmOffsetY/-20|saveas/'.$saveas;
             } else {
-                $persistentOps = 'imageView2/1/w/480/h/270/interlace/1/q/90|imageView2/1/w/180/h/180/interlace/1/q/100';
+                $persistentOps = 'imageView2/1/w/480/h/270/interlace/1/q/100|imageView2/1/w/180/h/180/interlace/1/q/100';
             }
         }
         
